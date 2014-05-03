46012ef35fcf6071d21633942f8b77c81f8209ee
Gnuk <gnuk@mailoo.org>
2014-05-03 21:19:31 +0200

    Design types, change translations to yml

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new GNKWLDF\PokedexBundle\GNKWLDFPokedexBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/GNKWLDF/PokedexBundle/Controller/DefaultController.php
+++ src/GNKWLDF/PokedexBundle/Controller/DefaultController.php
@@ -13,7 +13,9 @@ namespace GNKWLDF\PokedexBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Gnkw\Symfony\HttpFoundation\FormattedResponse;
 
 /**
  * Default Pokedex Controller
@@ -22,6 +24,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class DefaultController extends Controller
 {
+    private $pokemonNumber = 719;
+
     /**
      * Standard action
      * @Route("/")
@@ -34,8 +38,31 @@ class DefaultController extends Controller
             'fr',
             'en'
         )));
-        $listtext = file_get_contents(__DIR__ . '/../data/pokemon/list.json');
-        $list = json_decode($listtext, true);
-        return array('number' => 719, 'list' => $list);
+        return array('number' => $this->pokemonNumber);
+    }
+    
+    /**
+     * Standard action
+     * @Route("/api/pokemon/description/{number}", name="pokedex_api_pokemon_description", options={"expose"=true})
+     * @Method("GET")
+     */
+    public function descriptionAction($number)
+    {
+        $number = intval($number);
+        $request = $this->getRequest();
+        $request->setLocale($request->getPreferredLanguage(array(
+            'fr',
+            'en'
+        )));
+        if(!is_int($number) OR $this->pokemonNumber < $number OR 0 > $number)
+        {
+            return new Response('Problem with number', 400);
+        }
+        $response = array(
+            'name' => $this->get('translator')->trans('pokemon.list.' . $number . '.name'),
+            'image' => $this->container->get('templating.helper.assets')->getUrl('bundles/gnkwldfpokedex/images/pokemon/normal/'.$number.'.jpg'),
+            'description' => null
+        );
+        return new FormattedResponse($response);
     }
 }
