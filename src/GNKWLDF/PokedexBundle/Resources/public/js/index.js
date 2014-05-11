f474975203f52cdd661e48d4246bc30f2bf56e0e
Gnuk <gnuk@mailoo.org>
2014-05-11 14:53:00 +0200

    Change icon list button colors refering #7

--- src/GNKWLDF/PokedexBundle/Resources/public/js/index.js
+++ src/GNKWLDF/PokedexBundle/Resources/public/js/index.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     $(".pokedex-action").click(function(event){
         event.preventDefault();
-        var pageurl = $(this).attr('href');
-        var number = $(this).attr("data-pokemon-number");
+        var pokemon = $(this);
+        var pageurl = pokemon.attr('href');
+        var number = pokemon.attr("data-pokemon-number");
         $.ajax({
             type: "GET",
             url: Routing.generate('pokedex_pokemon_description', { number: number }),
             success: function(data){
+                $('.pokedex-action .pokedex-element').removeClass( "selected" );
+                pokemon.children('.pokedex-element').addClass( "selected" );
                 $('#pokeview').html(data);
                 window.history.pushState({path:pageurl},null,pageurl);
             },
