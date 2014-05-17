f99c71e802c3dbaeeeea18cd75d6bd6c5be5816b
Gnuk <gnuk@mailoo.org>
2014-05-17 15:04:20 +0200

    Add loader to description when clicking on an icon, close #7

--- src/GNKWLDF/PokedexBundle/Resources/public/js/index.js
+++ src/GNKWLDF/PokedexBundle/Resources/public/js/index.js
@@ -5,17 +5,30 @@ $(document).ready(function(){
         var pageurl = pokemon.attr('href');
         var number = pokemon.attr("data-pokemon-number");
         var pokemonElement = pokemon.children('.pokedex-element');
+        var loader = true;
         if(!pokemonElement.hasClass('selected'))
         {
+            var description = $('#pokeview').html();
+            setTimeout(function(){
+                if(loader)
+                {
+                    $('#pokeview').html('<div class="loader"><p class="hide">Loading …</p></div>');
+                }
+            }, 500);
             $.ajax({
                 type: "GET",
                 url: Routing.generate('pokedex_pokemon_description', { number: number }),
                 success: function(data){
+                    loader = false;
                     $('.pokedex-action .pokedex-element').removeClass( "selected" );
                     pokemonElement.addClass( "selected" );
                     $('#pokeview').html(data);
                     window.history.pushState({path:pageurl},null,pageurl);
                 },
+                error: function(){
+                    loader = false;
+                    $('#pokeview').html(description);
+                }
             });
         }
     });
